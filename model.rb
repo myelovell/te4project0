ca15aa1e2f3d21a079751f8d5ceea8ea57fcc683
erikalbinsson02 <erik.albinsson@elev.ga.ntig.se>
2021-08-18 15:02:53 +0200

    Update model.rb

--- model.rb
+++ model.rb
@@ -1,5 +1,5 @@
 def get_classmate_data() 
-    db = SQLite3::Database.new("classlistbra.db")
+    db = SQLite3::Database.new("db/classlistbra.db")
     db.results_as_hash = true
     classmates = db.execute("SELECT * FROM classmates")
     return classmates
