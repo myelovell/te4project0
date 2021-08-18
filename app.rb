50a136cc03ff5b7039ee1cc9f161916ce9fc790c
Linus Mac <scubegames@gmail.com>
2021-08-18 14:55:24 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,25 +1,20 @@
-#2021-08-17
-require "sinatra"
-require "slim"
-require "sqlite3"
-require_relative "model.rb"
+require 'sqlite3'
 
-enable :sessions
+begin
+    db = SQLite3::Database.open "db/classlistbra.db"
 
-get("/") do 
-    slim(:layout)
-end
-
-get("/memory") do 
-    slim(:memoryGame)
-end 
-
-
-
-get("/") do
-end
-
-get("/catalog") do 
-    classmates = get_classmate_data()
-    slim(:"catalog", locals:{classmates:classmates})
+    stm = db.prepare "SELECT * FROM classmates" 
+    rs = stm.execute 
+    
+    rs.each do |row|
+        puts row.join "\s"
+    end
+    
+rescue SQLite3::Exception => e 
+    
+    puts "Exception occurred"
+    puts e
+    
+ensure
+    db.close if db
 end
